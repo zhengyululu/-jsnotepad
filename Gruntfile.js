79a0d15cd4d12eb67c52eb63f979fea042567c7f
zhengyululu <2637106664@qq.com>
2020-04-09 00:31:52 +0800

    index

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,18 @@ module.exports=function(grunt){
         uglify:{
             './dist/bundle.min.js':'dist/bundle.js'    
         },
+        useminPrepare: {
+          html: 'index.html',
+          options: {
+            dest: 'dist'
+          }
+        },
+        usemin: {
+          html: ['dist/index.html']
+        },
+        clean: {
+          end: ['dist/bundle.css', 'dist/bundle.js', '.tmp']
+        },
 
     });
     grunt.loadNpmTasks('grunt-contrib-csslint');
@@ -54,6 +66,7 @@ module.exports=function(grunt){
     grunt.loadNpmTasks('grunt-contrib-cssmin')
     grunt.loadNpmTasks('grunt-contrib-uglify')
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-usemin');
 
-    grunt.registerTask('default',['htmlhint', 'csslint','eslint','concat','htmlmin','cssmin','uglify'])
+    grunt.registerTask('default',['htmlhint', 'csslint','eslint', 'useminPrepare','concat','cssmin','uglify','usemin','htmlmin'])
 }
